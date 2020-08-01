4d4ce6f836893caecf1f128795f1ea8aad8eb040
LarzDosan <rey_gato@hotmail.com>
2020-08-01 00:17:52 -0400

    terminado la parte visual

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -4,3 +4,16 @@ import "../scss/styles.scss";
 //* import 'image.jpg';
 
 
+const links: NodeListOf<Element> = document.querySelectorAll('#links a');
+
+
+links.forEach(element => {
+    element.addEventListener('click', () => {
+        links.forEach(element => {
+            element.classList.remove('active');
+        })
+        element.classList.add('active');
+    })
+})
+
+
