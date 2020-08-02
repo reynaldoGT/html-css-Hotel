618d6d78c2ad5bc56c91c8ea72b102c3bc4e689a
LarzDosan <rey_gato@hotmail.com>
2020-08-01 20:29:23 -0400

    correciones y media querys agregados

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -2,7 +2,7 @@ import "../scss/styles.scss";
 //?Here go you ts code
 //* import '../static/image.jpg';
 //* import 'image.jpg';
-
+import { rooms_cards, roomsInterface } from './rooms-cards';
 
 const links: NodeListOf<Element> = document.querySelectorAll('#links a');
 
@@ -15,5 +15,22 @@ links.forEach(element => {
         element.classList.add('active');
     })
 })
+
+const rooms = document.querySelector('#rooms-cards');
+rooms_cards.forEach(room => {
+    const room_card = document.createElement('div');
+    room_card.classList.add('card');
+    room_card.innerHTML += ` <img src="${room.urlImage}" alt="">
+    <div class="card-info">
+        <div class="title-price">
+            <h2>${room.room_name}</h2>
+            <span>BOB ${room.price} / ${room.location}</span>
+        </div>
+        <div class="ver-detalles">
+            ver Detalles
+        </div>
+    </div>`;
+    rooms.appendChild(room_card);
+});
 
 
---
+++ src/ts/rooms-cards.ts
@@ -0,0 +1,45 @@
+export interface roomsInterface {
+    urlImage: String;
+    room_name: String;
+    price: Number;
+    location: String
+}
+
+export const rooms_cards: roomsInterface[] = [
+    {
+        urlImage: 'https://r-cf.bstatic.com/images/hotel/max1024x768/208/208086215.jpg',
+        room_name: 'Suit Master',
+        price: 200,
+        location: 'Norte'
+    },
+    {
+        urlImage: 'https://q-cf.bstatic.com/images/hotel/max1024x768/239/239163829.jpg',
+        room_name: 'Suit Normal',
+        price: 150,
+        location: 'South'
+    },
+    {
+        urlImage: 'https://2486634c787a971a3554-d983ce57e4c84901daded0f67d5a004f.ssl.cf1.rackcdn.com/indian-springs-calistoga/media/indiansprings-header-theviewrooms-5c4ba99cb73c7.jpg',
+        room_name: 'Suit Normal II',
+        price: 160,
+        location: 'west'
+    },
+    {
+        urlImage: 'https://q-cf.bstatic.com/images/hotel/max1024x768/239/239163829.jpg',
+        room_name: 'Suit Normal',
+        price: 150,
+        location: 'South'
+    },
+    {
+        urlImage: 'https://r-cf.bstatic.com/images/hotel/max1024x768/208/208086215.jpg',
+        room_name: 'Suit Master',
+        price: 200,
+        location: 'Norte'
+    },
+    {
+        urlImage: 'https://q-xx.bstatic.com/xdata/images/hotel/840x460/205684046.jpg?k=edd4b442376ac9cf1784cb06c8de0dc8a6037a871e50f060ba8bbcdc297cb74a&o=',
+        room_name: 'Suit Normal',
+        price: 150,
+        location: 'South'
+    },
+]
